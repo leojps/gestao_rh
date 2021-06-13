d0da62207aa2cfc910bc1b3e8d8122355d953be8
Leonardo Silva <leo.jps@gmail.com>
2021-06-13 20:51:45 -0300

    Requirementes

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -5,7 +5,7 @@ from apps.funcionarios.models import Funcionario
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
-    pertence = models.ForeignKey(Funcionario, on_delete=models.SET_NULL, null=True, blank=True)
+    pertence = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.descricao
--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -6,7 +6,7 @@ from apps.empresas.models import Empresa
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
     departamento = models.ManyToManyField(Departamento)
     empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
 
