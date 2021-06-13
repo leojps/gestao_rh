507ed0c1711b12a2d709f1817e9d081876193a19
Leonardo Silva <leo.jps@gmail.com>
2021-06-13 20:46:25 -0300

    Ajustes

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,8 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
+from apps.departamentos.models import Departamento
+from apps.empresas.models import Empresa
 
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    departamento = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nome
