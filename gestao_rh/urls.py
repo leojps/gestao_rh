7a42091aa48554db1b4093cc3caf2799a9062abe
Leonardo Silva <leo.jps@gmail.com>
2021-06-13 22:19:39 -0300

    login

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', include('apps.core.urls')),
     path('funcionarios/', include('apps.funcionarios.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
