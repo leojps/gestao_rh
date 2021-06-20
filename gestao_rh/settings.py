307c84cf7f5e4a9c7ceb4c2de6ce6f82f4c3ef8f
Leonardo Silva <leo.jps@gmail.com>
2021-06-20 15:29:52 -0300

    'collectstatics'

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -108,8 +108,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
