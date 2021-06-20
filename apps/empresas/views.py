76ddd6dcf02079ea648725b0bfe63c1e5520d160
Leonardo Silva <leo.jps@gmail.com>
2021-06-20 08:14:22 -0300

    Ajustes no CRUD

--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from apps.empresas.models import Empresa
 
-# Create your views here.
+
+class EmpresaCreate(CreateView):
+    model = Empresa
+    fields = ['nome']
